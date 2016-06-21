b0748450f4b60de0160a86ef03eec120a50a4688
iserge <s.serge.b@gmail.com>
2016-06-21 14:02:12 +0300

    Add set render vector mode with OpenLayers 3.16.0

--- gwt-olcs-main/src/main/java/org/ol3cesium/client/olx/layer/VectorTileLayerOptions.java
+++ gwt-olcs-main/src/main/java/org/ol3cesium/client/olx/layer/VectorTileLayerOptions.java
@@ -15,6 +15,9 @@
  */
 package org.ol3cesium.client.olx.layer;
 
+import org.ol3cesium.client.ol.layer.VectorTileLayerRenderType;
+import org.ol3cesium.client.ol.source.VectorTileSource;
+
 /**
  *
  * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
@@ -26,5 +29,50 @@ public class VectorTileLayerOptions extends VectorLayerOptions {
     
     public static final native VectorTileLayerOptions create() /*-{
         return {};
+    }-*/;
+
+    /**
+     * Render mode for vector tiles:
+     * 'image': Vector tiles are rendered as images. Great performance, but point 
+     * symbols and texts are always rotated with the view and pixels are scaled 
+     * during zoom animations.
+     * 'hybrid': Polygon and line elements are rendered as images, so pixels are 
+     * scaled during zoom animations. Point symbols and texts are accurately 
+     * rendered as vectors and can stay upright on rotated views.
+     * 'vector': Vector tiles are rendered as vectors. Most accurate rendering 
+     * even during animations, but slower performance than the other options. 
+     * The default is 'hybrid'.
+     * @param renderMode Render mode
+     */
+    public final native void setRenderMode(VectorTileLayerRenderType renderMode) /*-{
+        this.renderMode = renderMode.toString();
+    }-*/;
+    
+    /**
+     * Render mode for vector tiles:
+     * 'image': Vector tiles are rendered as images. Great performance, but point 
+     * symbols and texts are always rotated with the view and pixels are scaled 
+     * during zoom animations.
+     * 'hybrid': Polygon and line elements are rendered as images, so pixels are 
+     * scaled during zoom animations. Point symbols and texts are accurately 
+     * rendered as vectors and can stay upright on rotated views.
+     * 'vector': Vector tiles are rendered as vectors. Most accurate rendering 
+     * even during animations, but slower performance than the other options. 
+     * The default is 'hybrid'.
+     * @param renderMode Render mode
+     */
+    public final native void setRenderMode(String renderMode) /*-{
+        this.renderMode = renderMode;
+    }-*/;
+    
+//    public final native void setRenderOrder	function	experimental
+//    Render order. Function to be used when sorting features before rendering. By default features are drawn in the order that they are created. Required.
+
+    /**
+     * Source.
+     * @param source Source.
+     */
+    public final native void setSource(VectorTileSource source) /*-{
+        this.source = source;
     }-*/;
 }
